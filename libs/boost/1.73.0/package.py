ca1c356f2b9862b5d88539e8160adf37c2e6904c
KazenZhong <tcx19900712@163.com>
2021-08-18 15:04:45 +0800

    boost

---
+++ libs/boost/1.73.0/package.py
@@ -0,0 +1,42 @@
+import platform
+
+name = "boost"
+
+version = "1.73.0"
+
+authors = [
+    "Boost community"
+]
+
+description = \
+    """
+    Boost is a set of libraries for the C++ programming language 
+    that provides support for tasks and structures such as linear 
+    algebra, pseudorandom number generation, multithreading,
+    image processing, regular expressions, and unit testing.
+    """
+
+build_requires = [
+
+]
+
+requires = [
+    "python-3.7.10"
+]
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86-64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.boost"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.CMAKE_PREFIX_PATH.append("{root}/lib/cmake")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
