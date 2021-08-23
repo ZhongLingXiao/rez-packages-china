f0a7ac49440f3453ac8f4ca8e20e6da769228341
KazenZhong <tcx19900712@163.com>
2021-08-23 14:13:57 +0800

    Add tbb 2020.2

---
+++ libs/tbb/2020.2/package.py
@@ -0,0 +1,40 @@
+import platform
+
+name = "tbb"
+
+version = "2020.2"
+
+authors = [
+    "James Reinders",
+    "Rafael Asenjo",
+    "Michael J. Voss"
+]
+
+description = \
+    """
+    Intel® Threading Building Blocks (Intel® TBB) is a library that
+    supports scalable parallel programming using standard ISO C++ code.
+    """
+
+build_requires = [
+    "python-3.7.10"
+]
+
+requires = []
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86-64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.tbb"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.CMAKE_PREFIX_PATH.append("{root}/lib/cmake/{name}")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
