2b42ca150c7b5170dbccc54f11296bde86944394
KazenZhong <tcx19900712@163.com>
2021-08-26 14:22:30 +0800

    Add tinykazen suites

---
+++ suites/tinykazen/2021/package.py
@@ -0,0 +1,30 @@
+name = "tinykazen"
+
+version = "2021"
+
+authors = [
+    "ZhongLingXiao"
+]
+
+description = \
+    """
+    Bundle for tinykazen dev.
+    """
+
+requires = [
+    # TODO: non-std, however is convenient for development. Move these to other suite in final commit. 
+    "cmake-3.20",
+    "embree-3.13.0",
+    "enoki-0.0.1",
+    "fmt-7.1.3",   
+    "oiio-2.2.15",
+    
+    # CY2021 standard
+    "boost-1.73",
+    "ocio-2.0.1"
+    "openexr-2.4.3",
+    "python-3.7",
+    "tbb-2020.2",
+]
+
+uuid = "tinykazen"
